a70d27c4fd9e34666ee2b9c4c45f2f6f0e6368ed
Olga Murashova <om.dreyfus@gmail.com>
2023-01-20 16:11:39 -0500

    added shop.css

--- my-app/src/pages/shop/shop.jsx
+++ my-app/src/pages/shop/shop.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { PRODUCTS } from '../../products'; 
 import { Product } from "./product";
+import "./shop.css";
 
 const Shop = () => {
   return (
     <div className='shop '>
-      <div className='shoptTitle'>
+      <div className='shopTitle'>
         <h1>Online Store</h1>
       </div>
 
