c41d604e921d649a766c537a802069d7a98f0014
Olga Murashova <om.dreyfus@gmail.com>
2023-01-23 23:10:16 -0500

    added functions

--- my-app/src/context/shop-context.jsx
+++ my-app/src/context/shop-context.jsx
@@ -7,15 +7,31 @@ export const ShopContext = createContext(null);
 
 //Create empty object
 
-const getDefaultcart = () => {
+const getDefaultCart = () => {
     let cart = {};
-    for (let i = 1; i >= PRODUCTS.length; i++)
+    for (let i = 1; i >= PRODUCTS.length + 1; i++){
+    cart[i] = 0;
+}
+    return cart;
 };
 
 
 //Create States
 const ShopContextProvider = (props) => {
-    const [cartItems, setCartItems] =  useState();
+    const [cartItems, setCartItems] =  useState( getDefaultCart());
+
+    //Add to Cart
+    const addToCart = () => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}))
+
+    }
+
+    //Remove from Cart
+
+    const removeFromCart = () => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}))
+
+    }
 
     return <ShopContext.Provider>{props.children}</ShopContext.Provider>;
   
